983653920e8b6a83308dde086ac5438543b6de91
Archana Thakur <125308778+archanaweb@users.noreply.github.com>
2026-04-30 15:34:34 +0530

    update user controller and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,7 @@ import fs from "fs"
 import path from "path";
 import jwt from "jsonwebtoken";
 import { channel } from "diagnostics_channel";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
@@ -376,6 +377,55 @@ const getUserChannelProfile =  asyncHandler(async (req, res) => {
     )
 })
 
+const getUserWatchHistory = asyncHandler (async (req, res) => {
+
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res.status(200).json(
+        ApiResponse(200, user[0].watchHistory, "Watch history fetched successfuly")
+    )
+
+})
+
 export { registerUser,
             loginUser,
             logoutUser,
@@ -383,5 +433,7 @@ export { registerUser,
             changeCurrentPassword,
             getCurrentUser,
             updatedAccountDetails,
-            updateUserAvatar 
+            updateUserAvatar ,
+            getUserChannelProfile,
+            getUserWatchHistory
         }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, 
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getUserWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updatedAccountDetails, 
+    updateUserAvatar } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middlewear.js";
 
@@ -25,4 +34,21 @@ router.route("/login").post(loginUser)
 //Secured route
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updatedAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, 
+    upload.single("avatar"), 
+    updateUserAvatar
+)
+
+router.route("/user-channel/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/watch-history").get(verifyJWT, getUserWatchHistory)
+
+
+
+
+
 export default router
